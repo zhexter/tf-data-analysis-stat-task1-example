f51cd21aaff551eb08a129ed4380df23dbe8d81c
Eugenia <zhexter@gmail.com>
2023-04-04 10:43:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 12162367 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    x = x * 2 / (99 * 99)
     return x.mean() # Ваш ответ
